49d354b5057691452fc6fe9c42e26771c7e661ab
eldo88 <leegjones88@gmail.com>
2023-12-09 19:18:26 -0700

    Moved variables from car to vehicle

--- Car.cs
+++ Car.cs
@@ -5,26 +5,11 @@ namespace vehicle_app;
 
 public sealed class Car : Vehicle
 {
-    public string EngineType;
-    public int? MPG;
-    public int? Range;
-    public int? fuelCapacity = 18;
-    public Car(string color, int capacity, string make, string model, int year, VehicleType vehicleType, string engineType, int mpg) : base(color, capacity, make, model, year, vehicleType)
-    {
-        if (string.IsNullOrWhiteSpace(engineType))
-            {throw new ArgumentException("Engine type is required");}
-        EngineType = engineType;
+    // public int? fuelCapacity = 18;
 
-        if (engineType != "Electric")
-        {
-            MPG = mpg;
-            Range = fuelCapacity * MPG;
-        }
-        else
-        {
-            Range = 350;
-            fuelCapacity = 0;
-        }
+    public Car(string color, int capacity, string make, string model, int year, VehicleType vehicleType, string engineType, int mpg) : base(color, capacity, make, model, year, vehicleType, engineType, mpg)
+    {
+
     }
 
     public override Dictionary<string, decimal> Drive()
--- Vehicle.cs
+++ Vehicle.cs
@@ -3,7 +3,7 @@ namespace vehicle_app;
 public abstract class Vehicle
 {
 
-    public Vehicle(string color, int capacity, string make, string model, int year, VehicleType vehicleType)
+    public Vehicle(string color, int capacity, string make, string model, int year, VehicleType vehicleType, string engineType, int mpg)
     {
         if (string.IsNullOrWhiteSpace(color))
         {throw new ArgumentException("The color is required.");}
@@ -12,24 +12,43 @@ public abstract class Vehicle
         Capacity = capacity;
 
         if (string.IsNullOrWhiteSpace(make))
-        {throw new ArgumentException("The vehicle make is required.");}
+            {throw new ArgumentException("The vehicle make is required.");}
         Make = make;
 
         if (string.IsNullOrWhiteSpace(model))
-        {throw new ArgumentException("The vehicle model is required.");}
+            {throw new ArgumentException("The vehicle model is required.");}
         Model = model;
 
         Year = year;
 
         VehicleType = vehicleType;
+
+        if (string.IsNullOrWhiteSpace(engineType))
+            {throw new ArgumentException("Engine type is required");}
+        EngineType = engineType;
+
+        if (engineType != "Electric")
+        {
+            MPG = mpg;
+            Range = FuelCapacity * MPG;
+        }
+        else
+        {
+            Range = 350;
+            FuelCapacity = 0;
+        }
     }
 
     public string? Color {get; set;}
     public int? Capacity {get; set;}
-    public string? Make {get; set;}
-    public string? Model {get; set;}
+    public string Make {get; set;}
+    public string Model {get; set;}
     public int? Year {get; set;}
     public VehicleType VehicleType {get;}
+    public string EngineType {get; set;}
+    public decimal? MPG {get; set;}
+    public decimal? Range {get; set;}
+    public decimal? FuelCapacity {get; set;} 
 
 
     public virtual Dictionary<string, decimal> Drive()
