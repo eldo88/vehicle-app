fa74a554e54b7019637fb71bb4bcea5ccffecc71
eldo88 <leegjones88@gmail.com>
2024-02-03 10:01:32 -0700

    Added EngineFactory

---
+++ Vehicle/Engine/EngineFactory.cs
@@ -0,0 +1,19 @@
+﻿namespace vehicle_app;
+
+public class EngineFactory
+{
+    public static IGasEngine CreateGasEngine(int numberOfCylinders, bool hasTurbo)
+    {
+        return new GasEngine(numberOfCylinders, hasTurbo);
+    }
+
+    public static IElectricMotor CreateElectricMotor()
+    {
+        return new ElectricMotor();
+    }
+
+    public static IDieselEngine CreateDieselEngine(int numberOfCylinders)
+    {
+        return new DieselEngine(numberOfCylinders);
+    }
+}
