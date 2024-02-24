a42befde3402c07bb437e16490fc8ce6377b94d6
eldo88 <leegjones88@gmail.com>
2024-02-24 15:55:42 -0700

    Added json as data format type and implemented SaveVehicle method to use json for suv repo

--- Vehicle/Repositories/SuvRepository.cs
+++ Vehicle/Repositories/SuvRepository.cs
@@ -1,4 +1,6 @@
 
+using System.Text.Json;
+
 namespace vehicle_app;
 
 internal class SuvRepository : IVehicleRepository<Suv>
@@ -24,30 +26,78 @@ internal class SuvRepository : IVehicleRepository<Suv>
         return suvs.FindAll(s => s.Model == model);
     }
 
+    // public List<Suv> GetVehicles()
+    // {
+    //     List<Suv> suvs = new();
+    //     FileOperations.ReadDataFromMockDbFile(MockDbFilePath, _suvs);
+        
+    //     foreach (var line in _suvs)
+    //     {
+    //         var capacity = int.Parse(line[2]);
+    //         var year = int.Parse(line[5]);
+    //         var mpg = int.Parse(line[8]);
+    //         Suv suv = (Suv)VehicleFactory.Build(line[1], capacity, line[3], line[4], year, line[6], line[7], mpg);
+    //         suvs.Add(suv);
+    //     }
+
+    //     return suvs;
+    // }
+
     public List<Suv> GetVehicles()
     {
-        List<Suv> suvs = new();
-        FileOperations.ReadDataFromMockDbFile(MockDbFilePath, _suvs);
-        
-        foreach (var line in _suvs)
-        {
-            var capacity = int.Parse(line[2]);
-            var year = int.Parse(line[5]);
-            var mpg = int.Parse(line[8]);
-            Suv suv = (Suv)VehicleFactory.Build(line[1], capacity, line[3], line[4], year, line[6], line[7], mpg);
-            suvs.Add(suv);
-        }
+        var filePath = "./Vehicle/Repositories/SavedData/suvs-saved.json";
+        using StreamReader fileReader = new(File.OpenRead(filePath));
+        var jd = fileReader.ReadToEnd();
 
+        List<Suv>? suvs = new();
+
+        if (!string.IsNullOrWhiteSpace(jd))
+        {
+            suvs = JsonSerializer.Deserialize<List<Suv>>(jd);
+        }
+        
+        if (suvs is null)
+        {
+            throw new NullReferenceException();
+        }
         return suvs;
     }
 
+    // public void SaveVehicle(Suv suv)
+    // {
+    //     var filePath = "../data/vehicle-data/suvs-saved.csv";
+    //     var suvData = FormatData.ParseVehicleDataForSavingToFile(suv);
+
+    //     using StreamWriter streamWriter = File.AppendText(filePath);
+    //     FileOperations.WriteDataToFile(streamWriter, suvData);
+    // }
+
     public void SaveVehicle(Suv suv)
     {
-        var filePath = "../data/vehicle-data/suvs-saved.csv";
-        var suvData = FormatData.ParseVehicleDataForSavingToFile(suv);
+        var filePath = "./Vehicle/Repositories/SavedData/suvs-saved.json";
+        using StreamReader fileReader = new(File.OpenRead(filePath));
+        var jd = fileReader.ReadToEnd();
 
-        using StreamWriter streamWriter = File.AppendText(filePath);
-        FileOperations.WriteDataToFile(streamWriter, suvData);
+        List<Suv>? suvs = new();
+
+        if (!string.IsNullOrWhiteSpace(jd))
+        {
+            suvs = JsonSerializer.Deserialize<List<Suv>>(jd);
+        }
+
+        if (suvs is null)
+        {
+            suvs = new List<Suv>{suv};
+        }
+        else
+        {
+            suvs.Add(suv);
+        }
+        
+        var options = new JsonSerializerOptions(){WriteIndented=true};
+        var jsonData = JsonSerializer.Serialize<IList<Suv>>(suvs, options);
+        using StreamWriter streamWriter = new(filePath, false);
+        streamWriter.Write(jsonData);
     }
 
     public List<Suv> GetVehicleByMake(string make)
--- Vehicle/Repositories/TruckRepository.cs
+++ Vehicle/Repositories/TruckRepository.cs
@@ -25,20 +25,40 @@ internal class TruckRepository : IVehicleRepository<Truck>
         return trucks.FindAll(t => t.Model == model);
     }
 
+    // public List<Truck> GetVehicles()
+    // {
+    //     List<Truck> trucks = new();
+    //     FileOperations.ReadDataFromMockDbFile(MockDbFilePath, _trucks);
+        
+    //     foreach (var line in _trucks)
+    //     {
+    //         var capacity = int.Parse(line[2]);
+    //         var year = int.Parse(line[5]);
+    //         var mpg = int.Parse(line[8]);
+    //         var truck = (Truck)VehicleFactory.Build(line[1], capacity, line[3], line[4], year, line[6], line[7], mpg);
+    //         trucks.Add(truck);
+    //     }
+
+    //     return trucks;
+    // }
+
     public List<Truck> GetVehicles()
     {
-        List<Truck> trucks = new();
-        FileOperations.ReadDataFromMockDbFile(MockDbFilePath, _trucks);
-        
-        foreach (var line in _trucks)
-        {
-            var capacity = int.Parse(line[2]);
-            var year = int.Parse(line[5]);
-            var mpg = int.Parse(line[8]);
-            var truck = (Truck)VehicleFactory.Build(line[1], capacity, line[3], line[4], year, line[6], line[7], mpg);
-            trucks.Add(truck);
-        }
+        var filePath = "./Vehicle/Repositories/SavedData/trucks-saved.json";
+        using StreamReader fileReader = new(File.OpenRead(filePath));
+        var jd = fileReader.ReadToEnd();
 
+        List<Truck>? trucks = new();
+
+        if (!string.IsNullOrWhiteSpace(jd))
+        {
+            trucks = JsonSerializer.Deserialize<List<Truck>>(jd);
+        }
+        
+        if (trucks is null)
+        {
+            throw new NullReferenceException();
+        }
         return trucks;
     }
 
@@ -56,7 +76,7 @@ internal class TruckRepository : IVehicleRepository<Truck>
         var filePath = "./Vehicle/Repositories/SavedData/trucks-saved.json";
         using StreamReader fileReader = new(File.OpenRead(filePath));
         var jd = fileReader.ReadToEnd();
-        
+
         List<Truck>? trucks = new();
 
         if (!string.IsNullOrWhiteSpace(jd))
