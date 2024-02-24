562b4dde47bb100ae698cc9264c61b1f88be4c7e
eldo88 <leegjones88@gmail.com>
2024-02-24 16:01:22 -0700

    Added json as data format type and implemented SaveVehicle method to use json for car repo

--- Vehicle/Repositories/CarRepository.cs
+++ Vehicle/Repositories/CarRepository.cs
@@ -1,4 +1,6 @@
 
+using System.Text.Json;
+
 namespace vehicle_app;
 
 internal class CarRepository : IVehicleRepository<Car>
@@ -23,29 +25,77 @@ internal class CarRepository : IVehicleRepository<Car>
         return cars.FindAll(c => c.Model == model);
     }
 
+    // public List<Car> GetVehicles()
+    // {
+    //     List<Car> cars = new();
+    //     FileOperations.ReadDataFromMockDbFile(MockDbFilePath, _cars);
+
+    //     foreach (var line in _cars)
+    //     {
+    //         var capacity = int.Parse(line[2]);
+    //         var year = int.Parse(line[5]);
+    //         var mpg = int.Parse(line[8]);
+    //         Car car = (Car)VehicleFactory.Build(line[1], capacity, line[3], line[4], year, line[6], line[7], mpg);
+    //         cars.Add(car);
+    //     }
+
+    //     return cars;
+    // }
+
     public List<Car> GetVehicles()
     {
-        List<Car> cars = new();
-        FileOperations.ReadDataFromMockDbFile(MockDbFilePath, _cars);
+        var filePath = "./Vehicle/Repositories/SavedData/cars-saved.json";
+        using StreamReader fileReader = new(File.OpenRead(filePath));
+        var jd = fileReader.ReadToEnd();
 
-        foreach (var line in _cars)
+        List<Car>? cars = new();
+
+        if (!string.IsNullOrWhiteSpace(jd))
         {
-            var capacity = int.Parse(line[2]);
-            var year = int.Parse(line[5]);
-            var mpg = int.Parse(line[8]);
-            Car car = (Car)VehicleFactory.Build(line[1], capacity, line[3], line[4], year, line[6], line[7], mpg);
-            cars.Add(car);
+            cars = JsonSerializer.Deserialize<List<Car>>(jd);
         }
-
+        
+        if (cars is null)
+        {
+            throw new NullReferenceException();
+        }
         return cars;
     }
 
+    // public void SaveVehicle(Car car)
+    // {
+    //     var filePath = "./data/vehicle-data/cars-saved.csv";
+    //     var carData = FormatData.ParseVehicleDataForSavingToFile(car);
+
+    //     using StreamWriter streamWriter = File.AppendText(filePath);
+    //     FileOperations.WriteDataToFile(streamWriter, carData);
+    // }
+
     public void SaveVehicle(Car car)
     {
-        var filePath = "./data/vehicle-data/cars-saved.csv";
-        var carData = FormatData.ParseVehicleDataForSavingToFile(car);
+        var filePath = "./Vehicle/Repositories/SavedData/cars-saved.json";
+        using StreamReader fileReader = new(File.OpenRead(filePath));
+        var jd = fileReader.ReadToEnd();
 
-        using StreamWriter streamWriter = File.AppendText(filePath);
-        FileOperations.WriteDataToFile(streamWriter, carData);
+        List<Car>? cars = new();
+
+        if (!string.IsNullOrWhiteSpace(jd))
+        {
+            cars = JsonSerializer.Deserialize<List<Car>>(jd);
+        }
+
+        if (cars is null)
+        {
+            cars = new List<Car>{car};
+        }
+        else
+        {
+            cars.Add(car);
+        }
+        
+        var options = new JsonSerializerOptions(){WriteIndented=true};
+        var jsonData = JsonSerializer.Serialize<IList<Car>>(cars, options);
+        using StreamWriter streamWriter = new(filePath, false);
+        streamWriter.Write(jsonData);
     }
 }
