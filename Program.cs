59aa33bdfeb4c5e5cdd26134f1efdfae0da90ff3
eldo88 <leegjones88@gmail.com>
2024-06-02 17:58:23 -0600

    Refactoring to improve code

--- Program.cs
+++ Program.cs
@@ -9,23 +9,20 @@ class Program
 
         var menuChoices = showMenus.MenuChoices.MenuChoicesFromUserInput;
         if(ExitProgram.ExitProgramValidator(menuChoices)) return;
-        IMotorizedVehicle createdVehicle;
         var data = showMenus.GetMenuChoiceData();
 
         try
         {
-            createdVehicle = VehicleFactory.BuildFromMenuChoices(data);
+            var createdVehicle = VehicleFactory.BuildFromMenuChoices(data);
             DisplayCreatedVehicle.PrintToConsole(createdVehicle);
             var vehicleService = VehicleServiceFactory.CreateVehicleService();
             SaveMenu saveMenu = new();
             saveMenu.SaveVehicleEvent += vehicleService.OnVehicleSaved;
             saveMenu.SaveVehicleMenu(createdVehicle);
 
-            if (TakeVehicleOnDriveMenu.GoOnDrive(createdVehicle.Make, createdVehicle.Model))
-            {
-                var driveLength = DriveLengthScreen.EnterDriveLength();
-                DisplayCreatedVehicle.PrintDriveDetails(createdVehicle.Drive(driveLength));
-            }
+            if (!TakeVehicleOnDriveMenu.GoOnDrive(createdVehicle.Make, createdVehicle.Model)) return;
+            var driveLength = DriveLengthScreen.EnterDriveLength();
+            DisplayCreatedVehicle.PrintDriveDetails(createdVehicle.Drive(driveLength));
         }
         catch(Exception ex)
         {
--- Vehicle/Service/VehicleService.cs
+++ Vehicle/Service/VehicleService.cs
@@ -3,30 +3,30 @@ namespace vehicle_app;
 
 public class VehicleService : IVehicleService
 {
-    public void OnVehicleSaved(object? source, VehicleEvent vehicleEvent)
+    public void OnVehicleSaved(object? source, VehicleEvent? vehicleEvent)
     {
-        if (vehicleEvent is not null)
+        if (vehicleEvent is null) return;
+        switch (vehicleEvent.Vehicle?.VehicleTypeEnum)
         {
-            switch (vehicleEvent.Vehicle?.VehicleTypeEnum)
-            {
-                case VehicleTypeEnum.Truck:
+            case VehicleTypeEnum.Truck:
                 var truckRepository = VehicleRepositoryFactory.BuildTruckRepo();
                 var truck = (Truck)vehicleEvent.Vehicle;
                 truckRepository.SaveVehicle(truck);
                 break;
-                case VehicleTypeEnum.Car:
+            case VehicleTypeEnum.Car:
                 var carRepository = VehicleRepositoryFactory.BuildCarRepo();
                 var car = (Car)vehicleEvent.Vehicle;
                 carRepository.SaveVehicle(car);
                 break;
-                case VehicleTypeEnum.SUV:
+            case VehicleTypeEnum.SUV:
                 var suvRepository = VehicleRepositoryFactory.BuildSuvRepo();
                 var suv = (Suv)vehicleEvent.Vehicle;
                 suvRepository.SaveVehicle(suv);
                 break;
-                default:
+            case null:
                 break;
-            }
+            default:
+                throw new ArgumentOutOfRangeException();
         }
     }
 }
