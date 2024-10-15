48aecd9a53b4d3c1ec787bba701fa9b2cddc47ed
eldo88 <leegjones88@gmail.com>
2024-10-14 18:49:43 -0600

    Fixed formatting error

--- Vehicle/VehicleExtensions.cs
+++ Vehicle/VehicleExtensions.cs
@@ -4,8 +4,8 @@ public static class VehicleExtensions
 {
     public static string DetailedMessage(this Exception exception)
     {
-        return $"An error occured at {DateTime.Now} \n Here are the details " +
-               $"\n {exception.Message} +" +
+        return $"An error occured at {DateTime.Now} \n Here are the details:" +
+               $"\n {exception.Message}" +
                $"\n {exception.StackTrace}";
     }
 }
